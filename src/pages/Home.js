5ba6f6499020da04531c90b506680407b71ebd02
nejahneji <nejinejah6@gmail.com>
2023-02-02 23:17:46 +0100

    connect-supabase

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,6 @@
-
+import supabase from '../config/supabaseClient'
 const Home = () => {
-  
+  console.log(supabase,'*****');
  console.log(process.env);
   return (
     <div className="page home">
